3e2842f0834fa3b5bffdefacf40f8cf2ff19cd7b
João Paulo <jpcchaves@outlook.com>
2023-04-16 10:57:18 -0300

    first commit

---
+++ src/test/java/com/jpcchaves/softreaming/SoftreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpcchaves.softreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
