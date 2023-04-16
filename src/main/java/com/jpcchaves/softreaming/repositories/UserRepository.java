e6c3a4bd9e791c85a89a478856c87c27df95cf93
João Paulo <jpcchaves@outlook.com>
2023-04-16 13:44:08 -0300

    feat: add existis by username or email methods in user repository

--- src/main/java/com/jpcchaves/softreaming/repositories/UserRepository.java
+++ src/main/java/com/jpcchaves/softreaming/repositories/UserRepository.java
@@ -9,4 +9,8 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
