549cb33588e99d1e6b89840a1e7446610e4ab6e3
João Paulo <jpcchaves@outlook.com>
2023-04-16 11:17:58 -0300

    feat: add profile entity

---
+++ src/main/java/com/jpcchaves/softreaming/config/entities/Profile.java
@@ -0,0 +1,60 @@
+package com.jpcchaves.softreaming.config.entities;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
+@Entity
+@Table(name = "profiles")
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String imgUrl;
+    @CreatedDate
+    private String createdAt;
+
+    public Profile() {
+    }
+
+    public Profile(Long id, String name, String imgUrl, String createdAt) {
+        this.id = id;
+        this.name = name;
+        this.imgUrl = imgUrl;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+}
