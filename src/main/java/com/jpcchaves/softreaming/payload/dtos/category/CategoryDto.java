b11b5f983b7ecbcf330382754a107d357b16be34
João Paulo <jpcchaves@outlook.com>
2023-04-19 10:53:08 -0300

    feat: add category dto

--- src/main/java/com/jpcchaves/softreaming/payload/dtos/category/CategoryDto.java
+++ src/main/java/com/jpcchaves/softreaming/payload/dtos/category/CategoryDto.java
@@ -1,2 +1,31 @@
-package com.jpcchaves.softreaming.payload.dtos.category;public class CategoryDto {
+package com.jpcchaves.softreaming.payload.dtos.category;
+
+
+public class CategoryDto {
+    private Long id;
+    private String category;
+
+    public CategoryDto() {
+    }
+
+    public CategoryDto(Long id, String category) {
+        this.id = id;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
