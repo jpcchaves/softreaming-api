3f3772aa77a7d658325ba033649d3bbe2e1c0591
João Paulo <jpcchaves@outlook.com>
2023-04-19 14:11:24 -0300

    feat: add movie controller

---
+++ src/main/java/com/jpcchaves/softreaming/controllers/MovieController.java
@@ -0,0 +1,47 @@
+package com.jpcchaves.softreaming.controllers;
+
+import com.jpcchaves.softreaming.payload.dtos.movie.MovieDto;
+import com.jpcchaves.softreaming.services.impl.MovieServiceImpl;
+import jakarta.websocket.server.PathParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+
+    private final MovieServiceImpl service;
+
+    public MovieController(MovieServiceImpl service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<MovieDto> create(@RequestBody MovieDto movieDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(movieDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovieDto>> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieDto> getById(@PathParam("id") Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(service.getById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MovieDto> update(@PathParam("id") Long id, @RequestBody MovieDto movieDto){
+        return ResponseEntity.status(HttpStatus.OK).body(service.update(movieDto, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathParam("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
