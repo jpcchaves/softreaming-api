8f6705c0e94da71517edda15714dc886230ce27a
João Paulo <jpcchaves@outlook.com>
2023-04-18 22:11:38 -0300

    feat: add category entity

---
+++ src/main/java/com/jpcchaves/softreaming/entities/Category.java
@@ -0,0 +1,37 @@
+package com.jpcchaves.softreaming.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String category;
+
+    public Category() {
+    }
+
+    public Category(Long id, String category) {
+        this.id = id;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
