b29edf31173b3cd56f8f5263adf81a492e10cd4c
João Paulo <jpcchaves@outlook.com>
2023-04-20 11:13:49 -0300

    feat: add get profiles count by user id

--- src/main/java/com/jpcchaves/softreaming/repositories/ProfileRepository.java
+++ src/main/java/com/jpcchaves/softreaming/repositories/ProfileRepository.java
@@ -10,5 +10,8 @@ import java.util.Optional;
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
     List<Profile> findAllByUser_Id(Long userId);
+
     Optional<Profile> findByUser_IdAndId(Long userId, Long id);
+
+    Integer countAllByUser_Id(Long userId);
 }
