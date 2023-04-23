54d2b43a14bd095af36502a98cea9a7b4c8ef9c1
João Paulo <jpcchaves@outlook.com>
2023-04-23 18:20:27 -0300

    feat: add profile controller documentation

--- src/main/java/com/jpcchaves/softreaming/controllers/ProfileController.java
+++ src/main/java/com/jpcchaves/softreaming/controllers/ProfileController.java
@@ -2,6 +2,11 @@ package com.jpcchaves.softreaming.controllers;
 
 import com.jpcchaves.softreaming.payload.dtos.profile.ProfileDto;
 import com.jpcchaves.softreaming.services.ProfileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,26 +22,91 @@ public class ProfileController {
         this.service = service;
     }
 
+    @Operation(summary = "Creates a new profile",
+            description = "Creates a new profile by passing a JSON representation of the profile",
+            tags = {"Profile"},
+            responses = {
+                    @ApiResponse(description = "Created", responseCode = "201",
+                            content = @Content(schema = @Schema(implementation = ProfileDto.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @PostMapping
     public ResponseEntity<ProfileDto> create(@RequestBody ProfileDto profileDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(profileDto));
     }
 
+    @Operation(summary = "Gets a profiles list",
+            description = "Gets a list of all profiles related to the logged in user",
+            tags = {"Profile"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    array = @ArraySchema(schema = @Schema(implementation = ProfileDto.class))
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @GetMapping
     public ResponseEntity<List<ProfileDto>> getAll() {
         return ResponseEntity.status(HttpStatus.OK).body(service.getAll());
     }
 
+    @Operation(summary = "Gets a profile by ID",
+            description = "Gets a profile related to the logged in user by passing a profile ID",
+            tags = {"Profile"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = ProfileDto.class)
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<ProfileDto> getById(@PathVariable(value = "id") Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(service.getById(id));
     }
 
+    @Operation(summary = "Updates a profile",
+            description = "Updates a profile by passing a profile ID and it's JSON representation",
+            tags = {"Profile"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = ProfileDto.class)
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @PutMapping("/{id}")
     public ResponseEntity<ProfileDto> update(@PathVariable(value = "id") Long id, @RequestBody ProfileDto profileDto) {
         return ResponseEntity.status(HttpStatus.OK).body(service.update(profileDto, id));
     }
 
+    @Operation(summary = "Deletes a profile",
+            description = "Deletes a task by passing a profile ID",
+            tags = {"Profile"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         service.delete(id);
