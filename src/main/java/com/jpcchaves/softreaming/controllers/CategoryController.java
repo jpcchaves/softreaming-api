b6aff843edae06a208e44751289c5a7670c95442
João Paulo <jpcchaves@outlook.com>
2023-04-24 10:00:29 -0300

    feat: add category controller doc

--- src/main/java/com/jpcchaves/softreaming/controllers/CategoryController.java
+++ src/main/java/com/jpcchaves/softreaming/controllers/CategoryController.java
@@ -2,6 +2,11 @@ package com.jpcchaves.softreaming.controllers;
 
 import com.jpcchaves.softreaming.payload.dtos.category.CategoryDto;
 import com.jpcchaves.softreaming.services.impl.CategoryServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -20,28 +25,93 @@ public class CategoryController {
     }
 
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Creates a new category",
+            description = "Creates a new category by passing a JSON representation of the category",
+            tags = {"Category"},
+            responses = {
+                    @ApiResponse(description = "Created", responseCode = "201",
+                            content = @Content(schema = @Schema(implementation = CategoryDto.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @PostMapping
     public ResponseEntity<CategoryDto> create(@RequestBody CategoryDto categoryDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(categoryDto));
     }
 
     @GetMapping
+    @Operation(summary = "Gets a categories list",
+            description = "Gets a list of all categories",
+            tags = {"Category"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    array = @ArraySchema(schema = @Schema(implementation = CategoryDto.class))
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<List<CategoryDto>> getAll() {
         return ResponseEntity.ok(service.getAll());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Gets a category by ID",
+            description = "Gets a category by passing a category ID",
+            tags = {"Category"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = CategoryDto.class)
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<CategoryDto> getById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(service.getById(id));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Updates a category",
+            description = "Updates a category by passing a category ID and it's JSON representation",
+            tags = {"Category"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = CategoryDto.class)
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @PutMapping("/{id}")
     public ResponseEntity<CategoryDto> update(@PathVariable("id") Long id, @RequestBody CategoryDto categoryDto) {
         return ResponseEntity.ok(service.update(categoryDto, id));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Deletes a category",
+            description = "Deletes a category by passing a category ID",
+            tags = {"Category"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") Long id) {
         service.delete(id);
