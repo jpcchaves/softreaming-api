9c5df8d55020e3f3769bddab71501de60908cc42
João Paulo <jpcchaves@outlook.com>
2023-05-19 11:25:33 -0300

    feat: add exists by userId and ratingId

--- src/main/java/com/jpcchaves/softreaming/repositories/LineRatingRepository.java
+++ src/main/java/com/jpcchaves/softreaming/repositories/LineRatingRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LineRatingRepository extends JpaRepository<LineRating, Long> {
     List<LineRating> findAllByRating_Id(Long ratingId);
 
     Boolean existsByUserIdAndRating_Id(Long userId, Long ratingId);
+
+    Optional<LineRating> findByUserIdAndId(Long userId, Long ratingId);
+
 }
