f3f75f8c6bb37ba9dbbdb695b08018fedd00a40f
João Paulo <jpcchaves@outlook.com>
2023-05-23 11:16:45 -0300

    refactor: organizing code and adjusting docs

--- src/main/java/com/jpcchaves/softreaming/controllers/MovieController.java
+++ src/main/java/com/jpcchaves/softreaming/controllers/MovieController.java
@@ -126,111 +126,6 @@ public class MovieController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @Operation(summary = "Adds a movie rating",
-            description = "Adds a movie rating by passing a movie ID and a JSON representation of the rating",
-            tags = {"Movie"},
-            responses = {
-                    @ApiResponse(description = "Success", responseCode = "200",
-                            content = @Content(
-                                    schema = @Schema(implementation = ApiMessageResponseDto.class)
-                            )
-                    ),
-                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
-            }
-    )
-    @PatchMapping("/{id}/rating")
-    public ResponseEntity<ApiMessageResponseDto> addRating(@PathVariable("id") Long id,
-                                                           @Valid @RequestBody MovieRatingDto movieRatingDto) {
-        return ResponseEntity.status(HttpStatus.OK).body(service.addMovieRating(id,
-                movieRatingDto));
-    }
-
-    @Operation(summary = "Adds a movie category",
-            description = "Adds a movie category by passing a movie ID and a List with the categories IDs",
-            tags = {"Movie"},
-            responses = {
-                    @ApiResponse(description = "Success", responseCode = "200",
-                            content = @Content(
-                                    schema = @Schema(implementation = ApiMessageResponseDto.class)
-                            )
-                    ),
-                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
-            }
-    )
-    @PatchMapping("/{id}/add-category")
-    public ResponseEntity<ApiMessageResponseDto> addCategory(@PathVariable("id") Long id,
-                                                             @RequestBody CategoryRequestDto categoryRequestDto) {
-        return ResponseEntity.status(HttpStatus.OK).body(service.addCategory(id,
-                categoryRequestDto));
-    }
-
-    @Operation(summary = "Removes a movie category",
-            description = "Removes a movie category by passing a movie ID and a List with the categories IDs",
-            tags = {"Movie"},
-            responses = {
-                    @ApiResponse(description = "Success", responseCode = "200",
-                            content = @Content(
-                                    schema = @Schema(implementation = ApiMessageResponseDto.class)
-                            )
-                    ),
-                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
-            }
-    )
-    @PatchMapping("/{id}/remove-category")
-    public ResponseEntity<ApiMessageResponseDto> removeCategory(@PathVariable("id") Long id,
-                                                                @RequestBody CategoryRequestDto categoryRequestDto) {
-        return ResponseEntity.status(HttpStatus.OK).body(service.removeCategory(id,
-                categoryRequestDto));
-    }
-
-    @Operation(summary = "Gets a rating list of a movie",
-            description = "Gets a list of all rating of a movie by passing a movie ID",
-            tags = {"Movie"},
-            responses = {
-                    @ApiResponse(description = "Success", responseCode = "200",
-                            content = @Content(
-                                    array = @ArraySchema(schema = @Schema(implementation = RatingDto.class))
-                            )
-                    ),
-                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
-            }
-    )
-    @GetMapping("/{id}/rating")
-    public ResponseEntity<RatingDto> getMovieRating(@PathVariable("id") Long id) {
-        return ResponseEntity.status(HttpStatus.OK).body(service.getMovieRating(id));
-    }
-
-    @Operation(summary = "Updates a movie rating",
-            description = "Updates a movie rating by passing a movie ID and a JSON representation of the rating",
-            tags = {"Movie"},
-            responses = {
-                    @ApiResponse(description = "Success", responseCode = "200",
-                            content = @Content(
-                                    schema = @Schema(implementation = ApiMessageResponseDto.class)
-                            )
-                    ),
-                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
-            }
-    )
-    @PatchMapping("/{id}/rating/{ratingId}")
-    public ResponseEntity<ApiMessageResponseDto> updateRating(@PathVariable("id") Long id,
-                                                              @PathVariable("ratingId") Long ratingId,
-                                                              @Valid @RequestBody MovieRatingDto movieRatingDto) {
-        return ResponseEntity.status(HttpStatus.OK).body(service.updateRating(id,
-                ratingId,
-                movieRatingDto));
-    }
-
     @Operation(summary = "Filter movies",
             description = "Filters movies by passing a movie name or/and release date. If you dont pass any parameters, a list of movies will be returned",
             tags = {"Movie"},
@@ -272,6 +167,111 @@ public class MovieController {
     @GetMapping("/best-rated")
     public ResponseEntity<List<MovieByBestRatedDto>> filterByBestRated() {
         return ResponseEntity.status(HttpStatus.OK).body(service.sortByBestRating());
+    }
+
+    @Operation(summary = "Adds a movie rating",
+            description = "Adds a movie rating by passing a movie ID and a JSON representation of the rating",
+            tags = {"Movie - Rating"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = ApiMessageResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
+    @PatchMapping("/{id}/rating")
+    public ResponseEntity<ApiMessageResponseDto> addRating(@PathVariable("id") Long id,
+                                                           @Valid @RequestBody MovieRatingDto movieRatingDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.addMovieRating(id,
+                movieRatingDto));
+    }
+
+    @Operation(summary = "Gets a rating list of a movie",
+            description = "Gets a list of all rating of a movie by passing a movie ID",
+            tags = {"Movie - Rating"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    array = @ArraySchema(schema = @Schema(implementation = RatingDto.class))
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
+    @GetMapping("/{id}/rating")
+    public ResponseEntity<RatingDto> getMovieRating(@PathVariable("id") Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.getMovieRating(id));
+    }
+
+    @Operation(summary = "Updates a movie rating",
+            description = "Updates a movie rating by passing a movie ID and a JSON representation of the rating",
+            tags = {"Movie - Rating"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = ApiMessageResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
+    @PatchMapping("/{id}/rating/{ratingId}")
+    public ResponseEntity<ApiMessageResponseDto> updateRating(@PathVariable("id") Long id,
+                                                              @PathVariable("ratingId") Long ratingId,
+                                                              @Valid @RequestBody MovieRatingDto movieRatingDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.updateRating(id,
+                ratingId,
+                movieRatingDto));
+    }
+
+    @Operation(summary = "Adds a movie category",
+            description = "Adds a movie category by passing a movie ID and a List with the categories IDs",
+            tags = {"Movie - Category"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = ApiMessageResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
+    @PatchMapping("/{id}/add-category")
+    public ResponseEntity<ApiMessageResponseDto> addCategory(@PathVariable("id") Long id,
+                                                             @RequestBody CategoryRequestDto categoryRequestDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.addCategory(id,
+                categoryRequestDto));
+    }
+
+    @Operation(summary = "Removes a movie category",
+            description = "Removes a movie category by passing a movie ID and a List with the categories IDs",
+            tags = {"Movie - Category"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = ApiMessageResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
+    @PatchMapping("/{id}/remove-category")
+    public ResponseEntity<ApiMessageResponseDto> removeCategory(@PathVariable("id") Long id,
+                                                                @RequestBody CategoryRequestDto categoryRequestDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.removeCategory(id,
+                categoryRequestDto));
     }
 
 }
