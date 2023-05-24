2e932d432b69cd8979afeafe42cd4c78fda3f8ae
João Paulo <jpcchaves@outlook.com>
2023-05-24 13:54:50 -0300

    feat: add directors controller swagger docs

--- src/main/java/com/jpcchaves/softreaming/controllers/DirectorsController.java
+++ src/main/java/com/jpcchaves/softreaming/controllers/DirectorsController.java
@@ -5,6 +5,11 @@ import com.jpcchaves.softreaming.payload.dtos.directors.DirectorDto;
 import com.jpcchaves.softreaming.payload.dtos.directors.DirectorsPaginatedDto;
 import com.jpcchaves.softreaming.payload.dtos.movie.MovieResponseMinDto;
 import com.jpcchaves.softreaming.services.DirectorsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -26,27 +31,94 @@ public class DirectorsController {
         this.service = service;
     }
 
+    @Operation(summary = "Gets a directors list",
+            description = "Gets a list of all directors",
+            tags = {"Director"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    array = @ArraySchema(schema = @Schema(implementation = DirectorsPaginatedDto.class))
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @GetMapping
     public ResponseEntity<DirectorsPaginatedDto> getAll(Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK).body(service.getAll(pageable));
     }
 
+    @Operation(summary = "Gets a director by ID",
+            description = "Gets a director by passing a director ID",
+            tags = {"Director"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = DirectorDto.class)
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<DirectorDto> getById(@PathVariable("id") Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(service.getById(id));
     }
 
+    @Operation(summary = "Get all movies by director",
+            description = "Gets a list all movies by director",
+            tags = {"Director"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    array = @ArraySchema(schema = @Schema(implementation = DirectorsPaginatedDto.class))
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @GetMapping("/{id}/movies")
     public ResponseEntity<Set<MovieResponseMinDto>> getAllMoviesByDirector(@PathVariable("id") Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(service.getAllMoviesByDirector(id));
     }
 
-
+    @Operation(summary = "Creates a new director",
+            description = "Creates a new director by passing a JSON representation of the director",
+            tags = {"Director"},
+            responses = {
+                    @ApiResponse(description = "Created", responseCode = "201",
+                            content = @Content(schema = @Schema(implementation = ApiMessageResponseDto.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @PostMapping
     public ResponseEntity<ApiMessageResponseDto> create(@RequestBody DirectorDto directorDto) {
         return ResponseEntity.status(HttpStatus.OK).body(service.create(directorDto));
     }
 
+    @Operation(summary = "Updates a director",
+            description = "Updates a director by passing a director ID and it's JSON representation",
+            tags = {"Director"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = ApiMessageResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @PutMapping("/{id}")
     public ResponseEntity<ApiMessageResponseDto> update(
             @PathVariable("id") Long id,
