377d7410afee81871bd29cc4d125f9b0a3ced0cc
João Paulo <jpcchaves@outlook.com>
2023-06-02 11:24:26 -0300

    feat: add actor dto class

---
+++ src/main/java/com/jpcchaves/softreaming/payload/dtos/actor/ActorDto.java
@@ -0,0 +1,42 @@
+package com.jpcchaves.softreaming.payload.dtos.actor;
+
+public class ActorDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public ActorDto() {
+    }
+
+    public ActorDto(Long id,
+                    String firstName,
+                    String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
